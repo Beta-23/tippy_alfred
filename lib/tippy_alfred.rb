42303fd73e7de86df3cc107aa4a81e69c3689ea6
Beta-23 <alfred.dominguez@gmail.com>
2019-06-03 12:11:40 -0700

    Initial commit

---
+++ lib/tippy_alfred.rb
@@ -0,0 +1,6 @@
+require "tippy_alfred/version"
+
+module TippyAlfred
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tippy_alfred/version.rb
@@ -0,0 +1,3 @@
+module TippyAlfred
+  VERSION = "0.1.0"
+end
